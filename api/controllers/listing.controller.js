56e40e4a8b1cc7d2aa13e6da66e8d49a6a7e75ec
thewildwest12 <shihabsarker650@gmail.com>
2025-01-04 00:00:50 +0600

    feat/update-listing-api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,6 +10,28 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
 //search api route backend listing.route.js
 export const getListing = async (req, res, next) => {
   try {
