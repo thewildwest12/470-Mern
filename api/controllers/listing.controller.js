d0b3582d5c14a073eb9d0b099ce2038599806533
thewildwest12 <shihabsarker650@gmail.com>
2025-01-05 01:16:53 +0600

    feat/listing-sort-and-pagination

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -62,43 +62,69 @@ export const getListing = async (req, res, next) => {
   }
 };
 //seach api route backend listing.route.js->
+// export const getListings = async (req, res, next) => {
+//   try {
+//     const limit = parseInt(req.query.limit) || 9;
+//     const startIndex = parseInt(req.query.startIndex) || 0;
+//     let offer = req.query.offer;
+//     //default behaviours 
+
+//     if (offer === undefined || offer === 'false') {
+//       offer = { $in: [false, true] };
+//     }
+
+//     let furnished = req.query.furnished;
+
+//     if (furnished === undefined || furnished === 'false') {
+//       furnished = { $in: [false, true] };
+//     }
+
+//     let parking = req.query.parking;
+
+//     if (parking === undefined || parking === 'false') {
+//       parking = { $in: [false, true] };
+//     }
+
+//     let type = req.query.type;
+
+//     if (type === undefined || type === 'all') {
+//       type = { $in: ['sale', 'rent'] };
+//     }
+//     //default values
+//     const searchTerm = req.query.searchTerm || ''; //if no search ""
+
+//     const sort = req.query.sort || 'createdAt';
+
+//     const order = req.query.order || 'desc';
+//     //search
+//     const listings = await Listing.find({
+//       name: { $regex: searchTerm, $options: 'i' }, // regex backend mongodb search functionality can search at anywehere options i lowercas
+//       offer,
+//       furnished,
+//       parking,
+//       type,
+//     })
+//       .sort({ [sort]: order })
+//       .limit(limit)
+//       .skip(startIndex); //start from index given
+
+//     return res.status(200).json(listings);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer;
-    //default behaviours 
 
-    if (offer === undefined || offer === 'false') {
-      offer = { $in: [false, true] };
-    }
-
-    let furnished = req.query.furnished;
-
-    if (furnished === undefined || furnished === 'false') {
-      furnished = { $in: [false, true] };
-    }
-
-    let parking = req.query.parking;
-
-    if (parking === undefined || parking === 'false') {
-      parking = { $in: [false, true] };
-    }
-
-    let type = req.query.type;
-
-    if (type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent'] };
-    }
-    //default values
-    const searchTerm = req.query.searchTerm || ''; //if no search ""
-
+    
     const sort = req.query.sort || 'createdAt';
+    const order = req.query.order === 'asc' || req.query.order === 'desc' ? req.query.order : 'desc';
 
-    const order = req.query.order || 'desc';
-    //search
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' }, // regex backend mongodb search functionality can search at anywehere options i lowercas
+      name: { $regex: searchTerm, $options: 'i' },
       offer,
       furnished,
       parking,
@@ -106,10 +132,6 @@ export const getListings = async (req, res, next) => {
     })
       .sort({ [sort]: order })
       .limit(limit)
-      .skip(startIndex); //start from index given
+      .skip(startIndex);
 
-    return res.status(200).json(listings);
-  } catch (error) {
-    next(error);
-  }
-};
+
