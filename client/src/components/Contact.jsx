f3517ee93eaf1d8b30f1faf054687f1c448a0ae3
thewildwest12 <shihabsarker650@gmail.com>
2025-01-05 15:17:38 +0600

    feat/contact-owner-adding-toasttify-notification-popup

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
