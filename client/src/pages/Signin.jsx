6c352339ea86f6c09d7b7994f0a22e39a6b8dff9
thewildwest12 <shihabsarker650@gmail.com>
2025-01-06 03:53:40 +0600

    feat/signin-userslice-and-oauth

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,27 +1,35 @@
 // SignIn.jsx
-//rudaba Sehnum 
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-  return (
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
+(
     <div className='min-h-screen bg-neutral-900 py-16'>
       <div className='max-w-lg mx-auto px-4'>
         <h1 className='text-4xl text-center font-bold text-white mb-6'>Sign In</h1>
         <div className='w-24 h-1 bg-amber-400 mx-auto mb-8'></div>
         
         <div className='bg-neutral-800 p-8 rounded-2xl shadow-xl border border-neutral-700'>
-          <form onSubmit={} className='flex flex-col gap-4'>
+          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <input
               type='email'
               placeholder='Email'
               className='bg-neutral-900 border border-neutral-700 p-3 rounded-xl text-white placeholder-neutral-400 focus:outline-none focus:border-amber-400 transition-colors'
               id='email'
-              onChange={}
+              onChange={handleChange}
             />
             <input
               type='password'
               placeholder='Password'
               className='bg-neutral-900 border border-neutral-700 p-3 rounded-xl text-white placeholder-neutral-400 focus:outline-none focus:border-amber-400 transition-colors'
               id='password'
-              onChange={}
+              onChange={handleChange}
             />
 
             <button
