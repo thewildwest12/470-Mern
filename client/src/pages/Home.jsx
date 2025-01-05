24b22f0d703f2ff0a7f977e02cd5cb5374ff0060
thewildwest12 <shihabsarker650@gmail.com>
2025-01-06 02:35:44 +0600

    feature/home-page-shows-offer-sale-and-buy-at-a-glace

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,19 @@
-//FAHIM FOYSAL ABIR || ARAF AHMED
+
+
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import SwiperCore from 'swiper';
+import 'swiper/css/bundle';
+import ListingItem from '../components/ListingItem';
+
+export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+
 
 
 
@@ -174,3 +189,5 @@
     </div>
   );
 }
+
+
