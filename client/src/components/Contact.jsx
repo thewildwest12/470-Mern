d317e212834ec6b0ac58f235fa604544256b0011
thewildwest12 <shihabsarker650@gmail.com>
2025-01-06 02:43:43 +0600

    bugfix/contact-owner-fetching-email-and-property-name

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -75,7 +75,7 @@ export default function Contact({ listing }) {
           ></textarea>
 
           <Link
-            to={`mailto:${landlord.name}?subject=Regarding ${listing.name}&body=${message}`}
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
             className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
             onClick={handleSendMessage}
           >
