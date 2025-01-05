7acba26602630649b7424cc028131d71c23726a4
thewildwest12 <shihabsarker650@gmail.com>
2025-01-06 02:37:09 +0600

    feature/home-page-get-listings-for-sale

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,6 +37,17 @@ export default function Home() {
       }
     };
 
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
 
   return (
     <div className="min-h-screen bg-slate-50">
